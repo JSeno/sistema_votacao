f4473ca6eecdacab05b0d7732e6c1028107a80c5
Jefferson Seno <jefferson.seno@datacake.com.br>
2022-04-28 12:13:47 -0300

    Criando sistema de votacão

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sistema de votação
+
+Estarei criando um sistema de votação onde você escolhe que tipo de candidato e depois qual candidato você quer votar.
+A partir dai após alguns votos será mostrado na tela o candidato que mais teve votos.
---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Sistema de votação.
+"""
+
+tipos_candidatos = ['Presidente', 'Governador', 'Senador', 'Deputado Federal', 'Deputado Estadual', 'Deputado Distrital']
+nomes_candidatos = {'Presidente': 'Jair Bolsonaro', 'Presidente': 'Luis Inácio Lula da Silva',
+                     'Governador': 'João Dória', 'Governador': 'Marcio França',
+                     'Senador': 'Ricardo Goulart', 'Senador': 'Fernando Haddad',
+                     'Deputado Federal': 'Kim Kataguri', 'Deputado Federal': 'João Amoedo',
+                     'Deputado Estadual': '', 'Deputado Estadual': 'João Amoedo',                     
+}
+
